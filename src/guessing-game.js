487191994fc87555d2b797f1973602db7136c0e0
Maksim <111878146+r-u-f-u-s@users.noreply.github.com>
2023-09-06 14:38:27 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.num = this.max - this.min;
+        this.num = Math.floor(this.num / 2);
+        this.num = this.max - this.num;
+        return this.num;
     }
 
     lower() {
-
+        this.max = this.num;
     }
 
     greater() {
-
-        
+        this.min = this.num;
     }
 }
 
